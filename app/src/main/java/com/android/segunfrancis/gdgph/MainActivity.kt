fa9ff3cbd232a39713de8c163799d2448440680d
segunfrancis <francis4dblues@gmail.com>
2019-11-11 01:12:50 +0100

    GDG-PH: implementation: ViewPager2 - register page change listener

--- app/src/main/java/com/android/segunfrancis/gdgph/MainActivity.kt
+++ app/src/main/java/com/android/segunfrancis/gdgph/MainActivity.kt
@@ -2,10 +2,10 @@ package com.android.segunfrancis.gdgph
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.viewpager2.widget.ViewPager2
 import com.android.segunfrancis.gdgph.adapter.PagerAdapter
-import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +19,21 @@ class MainActivity : AppCompatActivity() {
     private fun setUpViewPager() {
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
         val fragmentList = arrayListOf(OnBoardingFragment1.newInstance(), OnBoardingFragment2.newInstance())
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
         viewPager.adapter = PagerAdapter(this@MainActivity, fragmentList)
+
+        radioGroup.setOnCheckedChangeListener { group, i ->
+            val checkedButton = group.findViewById<RadioButton>(i)
+            val index = group.indexOfChild(checkedButton)
+            viewPager.setCurrentItem(index, true)
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                // when current page change -> update radio button state
+                val radioButtonId = radioGroup.getChildAt(position).id
+                radioGroup.check(radioButtonId)
+            }
+        })
     }
 }
